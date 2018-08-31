d368a8291e5e281ee456f1f7f3def8ec037c0a6f
Arsenault <earsenault@kflapublichealth.ca>
2018-08-31 11:11:34 -0400

    Python code - in various states.

---
+++ H_OCCPConvert.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+import scipy.io as sio
+folder_name = "J:\\Projects\\GIS\\climate2018\\rcp85_tasmin_daily\\"
+for entry in os.scandir(folder_name):
+    filename = entry.path
+    data = sio.loadmat(filename)
+    if data["outputData"].shape in {(8964,365,119)}: # 8964 grid squares * 365 days * 119 years, is norm for OCCP
+        np.save(filename[:-4],data["outputData"]) 
+    print("file: {}, shape: {}".format(filename,data["outputData"].shape))
+    os.remove(filename) # delete file on completion 
+
